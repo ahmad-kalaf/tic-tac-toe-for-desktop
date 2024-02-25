5ea0d892ebdbc007f706894e4b4e740221d664fa
Ahmad Kalaf <ahmad.it@outlook.de>
2024-02-25 13:12:46 +0100

    25.02.2024 13

--- src/GameGUI.java
+++ src/GameGUI.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -15,8 +16,8 @@ public class GameGUI implements ActionListener {
 	private JLabel _infoLabel;
 	private JPanel _fieldsPanel;
 	private JButton[] _fields;
+	private JButton _restartButton;
 	private String _next;
-	
 	
 	public GameGUI() {
 		_next = "X";
@@ -25,18 +26,19 @@ public class GameGUI implements ActionListener {
 		_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		_frame.setSize(350,420);
 		_frame.setTitle("Tic Tac Toe");
-		_frame.setResizable(true);
+		_frame.setResizable(false);
 		_frame.setLayout(new FlowLayout());
 		
 		_infoLabel = new JLabel();
 		_infoLabel.setBackground(Color.gray);
 		_infoLabel.setOpaque(true);
 		_infoLabel.setBorder(new LineBorder(Color.black, 2));
-		_infoLabel.setPreferredSize(new Dimension(300, 50));
+		_infoLabel.setPreferredSize(new Dimension(250, 50));
 		_infoLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		_infoLabel.setVerticalAlignment(SwingConstants.CENTER);
 		_infoLabel.setFont(new Font("Lato", Font.PLAIN, 40));
 		_infoLabel.setForeground(Color.white);
+		
 		_fieldsPanel = new JPanel(new GridLayout(3, 3));
 		_fieldsPanel.setPreferredSize(new Dimension(300,300));
 		_infoLabel.setText(_next + " ist dran");
@@ -53,8 +55,15 @@ public class GameGUI implements ActionListener {
 			_fieldsPanel.add(_fields[i]);
 		}
 		
+		_restartButton = new JButton("\u27F3");
+		_restartButton.addActionListener(this);
+		_restartButton.setPreferredSize(new Dimension(50, 50));
+		_restartButton.setFocusable(false);
+		
+		
 		_frame.add(_fieldsPanel);
 		_frame.add(_infoLabel);
+		_frame.add(_restartButton);
 
 		_frame.setVisible(true);
 		
@@ -63,13 +72,66 @@ public class GameGUI implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() instanceof JButton) {
+		if(e.getSource() == _restartButton) {
+			confirmClearFields();
+		} else if(e.getSource() instanceof JButton) {
 			JButton button = (JButton)e.getSource();
 			button.setText(_next);
 			button.setEnabled(false);
 			_next = _next.equals("X") ? "O" : "X";
 			_infoLabel.setText(_next + " ist dran");
 		}
+	}
+	
+	private void clearFields() {
+		for(int i = 0; i < _fields.length; ++i) {
+			_fields[i].setText("");
+			_fields[i].setEnabled(true);
+			_next = "X";
+			_infoLabel.setText("X ist dran");
+		}
+	}
+	
+	private void confirmClearFields() {
+		JDialog confirmDialog = new JDialog(_frame, "Neustart bestätigen", true);
+		
+		JButton yes = new JButton("Ja");
+		JButton no = new JButton("Nein");
+		
+		yes.setFocusable(false);
+		no.setFocusable(false);
+		
+		yes.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				clearFields();
+				confirmDialog.dispose();
+			}
+		});
+		
+		no.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				confirmDialog.dispose();
+			}
+		});
+		
+		JPanel yesNoPanel = new JPanel();
+		yesNoPanel.add(yes);
+		yesNoPanel.add(no);
+		
+		JLabel confirmQuestion = new JLabel("Spiel Neustarten?");
+		confirmQuestion.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		confirmDialog.setLayout(new BorderLayout());
+		confirmDialog.add(confirmQuestion, BorderLayout.CENTER);
+		confirmDialog.add(yesNoPanel, BorderLayout.SOUTH);
+		
+		confirmDialog.setSize(200,100);
+		confirmDialog.setLocationRelativeTo(_frame);
+		confirmDialog.setResizable(false);
+		
+		confirmDialog.setVisible(true);
 	}
 
 }
